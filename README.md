3894a88d7cf1e7f092b0a87118d9ee459adc4ee1
Gilly1970 <gilroyscott@hotmail.com>
2025-09-13 00:13:31 +1000

    make public

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # BPI-R4_OpenWrt_mtk-feeds_Build
 
+<<<<<<< HEAD
 **Build Openwrt with the latest kernel and mtk-openwrt-feeds, by default this will build all the basic image for the BPI-R4.**
 
 ## **To build with the latest kernel and mtk-feeds**
@@ -18,14 +19,45 @@
 	 * Inside each directory there is two files "openwrt-add-patch" and "openwrt-remove"
 	 * To add or remove a file or patch just enter the full target path into the file - target/linux/generic/backport-6.6/999-some.patch
 	 * The script will search each file at the start of the build and process all entries apply them to the targeted directories.. or removal
+=======
+Build with the latest Openwrt kernels and the latest mtk-openwrt-feeds...
+
+This is my latest evolving build script which incorperates the "rsync" function to improve the handling of scripts and patches. I've also include a new option to clone the main repos from a local repo. 
+
+## **To build with the latest kernel and mtk-openwrt-feeds**
+
+1. You can change branches "openwrt-24.10" , "master" , "main" etc...
+
+2. If you want to build with the latest openwrt-24.10 kernels and the latest mtk commits leave both OPENWRT_COMMIT="" & MTK_FEEDS_COMMIT="" empty.
+
+3. If you want to target a specific commit use the full commit hash e.g... OPENWRT_COMMIT="b4b9288f2aa3dd1a759e5effbc8378f614bd5755"
+
+4. I've add an optional function to clone from a local repo instead of pointing to the openwrt or mtk-openwrt-feeds repos to clone.
+	* saves a lot of time when testing but it's optional, default will clone from the online repos
+	 * I've added a new repo with a small shell script to automate the updating of both "openwrt" and "mtk-openwrt-feeds" local repositories, to keep them up to date.
+
+5. Added two new directories to place all patches and files into, one for "openwrt-patches" and the other for all "mtk-patches" 
+	 * Inside each direcory you drop in all your patches corresponding to the target (openwrt or mtk)
+	 * Inside each directory there is two files "openwrt-add-patch" and "openwrt-remove"
+	 * To add or remove a file or patch just enter the full target path into the file - target/linux/generic/backport-6.6/999-some.patch
+	 * The cp -f function works likes this.. "Some-Makefile:package/base-files/Makefile"
+	 * The mkdir -p function works like this.. Add the tree with the new dir  "Some-script.sh:files/etc/uci-defaults/new.sh" or "files/etc/uci-defaults/new.sh" in the correct add file.
+	 * The script will search each of the files at the start of the build and process all entries applying them to the targets.. (or removing them)
+>>>>>>> 54135fd (make public)
 	 
 6. You can use custom config files and scripts. 
 	 * Add any custom wireless, network config files to "/files/etc/config/" directory and it will be included into the built image.
 	 * Add any custom uci-defaults script into "openwrt-patches/files/etc/uci-defaults/" and it will be built into the image.
 
+<<<<<<< HEAD
 7. Added an option at the end of a successfully build, which will prompt the user if they want to enter into "make menuconfig".. optional.
      * When prompted either enter (yes/no): The default is 'no' or let it time out after 10 seconds and it will continue and existing the script.
 	 * If 'yes' enter into the make menuconfig and make any custom changes you need and save, it will continue into a new build creating new images with those changes.
+=======
+7. Added an option at the end of a successfully build, that will prompt the usre if they want to enter into "make menuconfig".. 
+	 * When prompted either enter (yes/no): The default is 'no' or let it time out after 10 seconds and it will continue and existing the script.
+	 * If 'yes' enter into the make menuconfig and make the changes you need and save, it will continue the build process and build a new images with your custom changes.
+>>>>>>> 54135fd (make public)
 
 8. Error Checks - All scripts and patches will be auto chacked with dos2unix and corrected if needed. 
 
@@ -33,12 +65,19 @@
 
 ## **How to Use**
 
+<<<<<<< HEAD
 1. **Prerequisites**: A Compatible build environment, such as **Ubuntu 24.04 LTS**. You will also need to install `dos2unix` & `rsync`:
    
    `sudo apt update`  
    `sudo apt install build-essential clang flex bison g++ gawk gcc-multilib g++-multilib 
    gettext git libncurses5-dev libssl-dev python3-distutils rsync unzip zlib1g-dev
    file wget dos2unix rsync`
+=======
+1. **Prerequisites**: Ensure you have a compatible build environment, such as **Ubuntu 24.04 LTS**. You will also need to install `dos2unix` & `rsync`:  
+   `sudo apt update` 
+   
+   `sudo apt install build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev python3-distutils rsync unzip zlib1g-dev file wget dos2unix rsync`
+>>>>>>> 54135fd (make public)
 
 2. **Clone repo**:
 
@@ -48,12 +87,39 @@
 
 3. **Run the Script**:  
    * Make the script executable:  
-     `chmod \+x mtk-openwrt_build.sh`
+     `chmod +x mtk-openwrt_build.sh`
      
    * Execute the script:  
      `./mtk-openwrt_build.sh`
 
 ## **Notes**
+<<<<<<< HEAD
 Please note - This script is still in development and is more than likely going to change at some point. If you like it the way it is make sure you have a backup copy :)
+=======
+Please Note - The "99999-mt7996-eeprom-fix-0s.patch" is my own modified version of both the "99999_tx_power_check.patch" & "9997-use-tx_power-from-default-fw-if-EEPROM-contains-0s.patch" patches.. I've combined the same logic from both patches and applied it into the one patch.
+
+root@OpenWrt:~# dmesg | grep mt7996
+[    5.359751] FIT:          flat_dt sub-image 0x00685000..0x006858f0 "fdt-mt7988a-bananapi-bpi-r4-wifi-mt7996a" (ARM64 OpenWrt bananapi_bpi-r4 device tree overlay mt7988a-bananapi-bpi-r4-wifi-mt7996a)
+[   14.564927] mt7996e_hif 0001:01:00.0: assign IRQ: got 124
+[   14.570344] mt7996e_hif 0001:01:00.0: enabling device (0000 -> 0002)
+[   14.576722] mt7996e_hif 0001:01:00.0: enabling bus mastering
+[   14.582486] mt7996e 0000:01:00.0: assign IRQ: got 121
+[   14.587545] mt7996e 0000:01:00.0: enabling device (0000 -> 0002)
+[   14.593556] mt7996e 0000:01:00.0: enabling bus mastering
+[   14.684236] mt7996e 0000:01:00.0: attaching wed device 0 version 3.0
+[   14.815063] mt7996e_hif 0001:01:00.0: attaching wed device 1 version 3.0
+[   15.144509] mt7996e 0000:01:00.0: HW/SW Version: 0x8a108a10, Build Time: 20250904203308a
+[   15.250373] mt7996e 0000:01:00.0: WM Firmware Version: ____000000, Build Time: 20250904203304
+[   15.295309] mt7996e 0000:01:00.0: DSP Firmware Version: ____000000, Build Time: 20250904202814
+[   15.316818] mt7996e 0000:01:00.0: WA Firmware Version: ____000000, Build Time: 20250904203218
+[   16.208804] mt7996e 0000:01:00.0: corrupted EEPROM detected, forcing default
+[   16.232865] mt7996e 0000:01:00.0: registering led 'mt76-phy0'
+[   31.777052] mt7996e 0000:01:00.0: Platform_type = 2, bypass_rro = 1, txfree_path = 0
+[   36.642242] mt7996e 0000:01:00.0 phy0.1-ap0: entered allmulticast mode
+[   36.648927] mt7996e 0000:01:00.0 phy0.1-ap0: entered promiscuous mode
+[   36.795098] mt7996e 0000:01:00.0: mt76_add_chanctx: add 100 on mt76 band 1
+
+
+>>>>>>> 54135fd (make public)
 
 
--- mtk-openwrt_build.sh
+++ mtk-openwrt_build.sh
@@ -2,42 +2,54 @@
 # ==================================================================================
 # BPI-R4 - OpenWrt with MTK-Feeds Build Script (Two-Stage Autobuild + Make)
 # ==================================================================================
-# Please Note - IF you use the custom setup scripts for 'uci-defaults'.. As a precaution
-#                 they will be auto convert with the dos2unix tool to correct any DOS line
-#                 endings that may be present. Some users edit in windows and pass the
-#                 files across to the build system, which can causes errors in unix based
-#                 systems.
+# This script uses list files to manage custom content.
+# - To add/overwrite a file: Place it in 'openwrt-patches' or 'mtk-patches'.
+#   For most files, list the destination path in the 'add-patch' file.
+#   For filename conflicts, use the 'source_filename:destination_path' format.
+# - To remove a file: Add its path to the corresponding 'remove' file.
+# - Custom runtime configs (uci-defaults, etc.) go in the 'files' directory.
+#
 # Build system Install Note  - Run on Ubuntu 24.04 or later
 #                            - sudo apt update
-#                            - sudo apt install dos2unix.
+#                            - sudo apt install dos2unix rsync patch
 # Usage:
 #
-#   ./build_mtk.sh
-#   ./build_mtk.sh -b openwrt-24.10
+#   ./mtk-openwrt_build.sh
+#   ./mtk-openwrt_build.sh -b openwrt-24.10
 #
 # ==================================================================================
 
 set -euo pipefail
 
 # --- Dependency Check ---
+<<<<<<< HEAD
 if ! command -v dos2unix &> /dev/null || ! command -v rsync &> /dev/null; then
     echo "ERROR: One or more dependencies (dos2unix, rsync) are not installed." >&2
     echo "Please run 'sudo apt update && sudo apt install dos2unix rsync'." >&2
+=======
+if ! command -v dos2unix &> /dev/null || ! command -v rsync &> /dev/null || ! command -v patch &> /dev/null; then
+    echo "ERROR: One or more dependencies (dos2unix, rsync, patch) are not installed." >&2
+    echo "Please run 'sudo apt update && sudo apt install dos2unix rsync patch'." >&2
+>>>>>>> 54135fd (make public)
     exit 1
 fi
+
 
 # --- Main Configuration ---
 
 # OpenWrt Source Details
+# --- Use this line for remote cloning ---
 readonly OPENWRT_REPO="https://git.openwrt.org/openwrt/openwrt.git"
-# --- Can use local repo (uncomment and set your path) ---
+# --- Use this line for local testing (uncomment and set your path) ---
 #readonly OPENWRT_REPO="/home/user/repos/openwrt"
 
 OPENWRT_BRANCH="openwrt-24.10"
 readonly OPENWRT_COMMIT=""
 
+# Mediatek Feeds Source Details
+# --- Use this line for remote cloning ---
 readonly MTK_FEEDS_REPO="https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds"
-# --- Can use local repo (uncomment and set your path) ---
+# --- Use this line for local testing (uncomment and set your path) ---
 #readonly MTK_FEEDS_REPO="/home/user/repos/mtk-openwrt-feeds"
 
 readonly MTK_FEEDS_BRANCH="master"
@@ -59,6 +71,7 @@ readonly SCRIPT_EXECUTABLE_NAME=$(basename "$0")
 
 
 # --- Functions ---
+
 show_usage() {
     echo "Usage: $SCRIPT_EXECUTABLE_NAME [-b <branch_name>]"
     echo "  -b <branch_name>  Specify the OpenWrt branch to build."
@@ -173,39 +186,67 @@ remove_files_from_list() {
     fi
 }
 
+# --- Applies files from a source directory based on a hybrid list file ---
 apply_files_from_list() {
     local list_file=$1
     local source_dir=$2
     local target_dir=$3
     local name=$4
+
     log "--- Applying $name files and patches from list '$list_file' ---"
     if [ ! -f "$list_file" ]; then
         log "Add list '$list_file' not found. Skipping."
         return
     fi
+
     local lines_processed=0
-    while IFS= read -r dest_relative_path; do
-        dest_relative_path=$(echo "$dest_relative_path" | tr -d '\r' | sed 's|^/||')
-        if [ -z "$dest_relative_path" ]; then continue; fi
+    while IFS= read -r line; do
+        # Skip comments and empty lines
+        [[ "$line" =~ ^\s*# ]] || [ -z "$line" ] && continue
+        
         lines_processed=$((lines_processed + 1))
-        local filename
-        filename=$(basename "$dest_relative_path")
-        local source_file="$source_dir/$filename"
-        local dest_file="$target_dir/$dest_relative_path"
-        if [ ! -f "$source_file" ]; then
-            log "($name) Warning: Source file '$filename' not found in '$source_dir'. Skipping."
+
+        local source_filename
+        local dest_relative_path
+
+        # Check if the line uses the 'source:destination' format
+        if [[ "$line" == *":"* ]]; then
+            source_filename=$(echo "$line" | cut -d':' -f1 | tr -d '[:space:]')
+            dest_relative_path=$(echo "$line" | cut -d':' -f2- | tr -d '[:space:]' | sed 's|^/||')
+        else
+            # Fallback to the old format (destination only)
+            dest_relative_path=$(echo "$line" | tr -d '[:space:]' | sed 's|^/||')
+            source_filename=$(basename "$dest_relative_path")
+        fi
+
+        if [ -z "$source_filename" ] || [ -z "$dest_relative_path" ]; then
+            log "($name) Warning: Malformed line found in '$list_file': '$line'. Skipping."
             continue
         fi
-        log "($name) Copying '$filename' to '$dest_relative_path'..."
+
+        local source_file="$source_dir/$source_filename"
+        local dest_file="$target_dir/$dest_relative_path"
+
+        if [ ! -f "$source_file" ]; then
+            log "($name) ERROR: Source file '$source_filename' not found in '$source_dir'. Skipping."
+            continue
+        fi
+
+        log "($name) Copying '$source_filename' to '$dest_relative_path'..."
+        
         local dest_dir
         dest_dir=$(dirname "$dest_file")
         mkdir -p "$dest_dir"
+
         cp "$source_file" "$dest_file"
+
     done < <(grep -v -E '^\s*#|^\s*$' "$list_file")
+
     if [ "$lines_processed" -eq 0 ]; then
         log "No files listed for application in '$list_file'."
     fi
 }
+
 
 copy_custom_files() {
     local source_dir="$SOURCE_CUSTOM_FILES_DIR"
@@ -243,7 +284,7 @@ configure_build() {
 
 prompt_for_custom_build() {
     log "--- Optional: Custom Image Creation ---"
-    echo "The base image has been built successfully using the autobuilder."
+    echo "The base image has been built successfully."
     echo "Would you like to create a custom image?"
     echo "You have 10 seconds to answer. The default is 'no'."
     local custom_choice=""
@@ -270,6 +311,13 @@ prompt_for_custom_build() {
 
             case "${build_choice,,}" in
                 y|yes)
+                    log "Removing old images from the output directory..."
+                    local image_dir="bin/targets/mediatek/filogic"
+                    if [ -d "$image_dir" ]; then
+                        rm -f "$image_dir"/*
+                        log "Old images removed."
+                    fi
+                    
                     log "Starting the custom build with 'make -j\$(nproc)'..."
                     make -j"$(nproc)"
                     log "--- Custom build process finished successfully! ---"
@@ -300,11 +348,13 @@ main() {
     log "--- Starting Full Build Setup ---"
     require_command "git" "awk" "make" "dos2unix" "rsync" "patch"
 
+    # --- Step 1: Repo Setup ---
     openwrt_commit=$( [ -n "$OPENWRT_COMMIT" ] && echo "$OPENWRT_COMMIT" || get_latest_commit_hash "$OPENWRT_REPO" "$OPENWRT_BRANCH" )
     setup_repo "$OPENWRT_REPO" "$OPENWRT_BRANCH" "$openwrt_commit" "$OPENWRT_DIR" "OpenWrt"
     mtk_feeds_commit=$( [ -n "$MTK_FEEDS_COMMIT" ] && echo "$MTK_FEEDS_COMMIT" || get_latest_commit_hash "$MTK_FEEDS_REPO" "$MTK_FEEDS_BRANCH" )
     setup_repo "$MTK_FEEDS_REPO" "$MTK_FEEDS_BRANCH" "$mtk_feeds_commit" "$MTK_FEEDS_DIR" "MTK Feeds"
 
+    # --- Step 2: Initialize Feeds ---
     (
         cd "$OPENWRT_DIR"
         log "Adding local Mediatek feeds to feeds configuration..."
@@ -316,17 +366,21 @@ main() {
         ./scripts/feeds install -a
     )
 
+    # --- Step 3: Prepare all custom source directories ---
     prepare_source_directory "$SOURCE_OPENWRT_PATCH_DIR" "OpenWrt Patches"
     prepare_source_directory "$SOURCE_MTK_FEEDS_PATCH_DIR" "MTK Patches"
     prepare_source_directory "$SOURCE_CUSTOM_FILES_DIR" "Custom Files"
 
+    # --- Step 4: Remove and Apply Source Files ---
     remove_files_from_list "$OPENWRT_REMOVE_LIST" "$OPENWRT_DIR" "OpenWrt"
     remove_files_from_list "$MTK_REMOVE_LIST" "$MTK_FEEDS_DIR" "MTK"
     apply_files_from_list "$OPENWRT_ADD_LIST" "$SOURCE_OPENWRT_PATCH_DIR" "$OPENWRT_DIR" "OpenWrt"
     apply_files_from_list "$MTK_ADD_LIST" "$SOURCE_MTK_FEEDS_PATCH_DIR" "$MTK_FEEDS_DIR" "MTK"
     
+    # --- Step 5: Copy Custom Runtime Files ---
     copy_custom_files
 
+    # --- Step 6: Configure and Run Base Build ---
     configure_build
     
     log "--- Starting the MediaTek autobuild script for the base image... ---"
@@ -337,6 +391,7 @@ main() {
     log "--- Base build process finished successfully! ---"
     log "--- You can find the base images in '$OPENWRT_DIR/bin/targets/mediatek/filogic/' ---"
 
+    # --- Step 7: Offer the optional custom build ---
     (
         cd "$OPENWRT_DIR"
         prompt_for_custom_build
